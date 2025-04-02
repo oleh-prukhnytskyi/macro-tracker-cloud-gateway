dbb85dfdcb9dddb581ddda7d15f1d91c85b0eb77
Oleg Pruh <olegpruh1@gmail.com>
2025-04-02 23:14:09 +0100

    first commit

---
+++ src/main/java/com/olehprukhnytskyi/macrotrackercloudgateway/MacroTrackerCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.olehprukhnytskyi.macrotrackercloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacroTrackerCloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MacroTrackerCloudGatewayApplication.class, args);
+    }
+
+}
