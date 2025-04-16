29c2e75abbfa82bd277d5c35e54c5c5505ecdc41
Oleg Pruh <olegpruh1@gmail.com>
2025-04-16 12:41:33 +0100

    Add userId to header and cover with tests

--- src/main/java/com/olehprukhnytskyi/macrotrackercloudgateway/util/JwtUtil.java
+++ src/main/java/com/olehprukhnytskyi/macrotrackercloudgateway/util/JwtUtil.java
@@ -1,6 +1,8 @@
 package com.olehprukhnytskyi.macrotrackercloudgateway.util;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import java.nio.charset.StandardCharsets;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -20,5 +22,18 @@ public class JwtUtil {
         } catch (Exception e) {
             return false;
         }
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public Long extractUserId(String token) {
+        Claims claims = extractAllClaims(token);
+        return claims.get("id", Long.class);
     }
 }
