2f18f41bd77e52d3b4f62d22542ac681f383c6bc
Oleg Pruh <olegpruh1@gmail.com>
2025-11-20 00:06:12 +0000

    Remove logging and tracing filter

--- src/main/java/com/olehprukhnytskyi/macrotrackercloudgateway/util/CustomHeaders.java
+++ src/main/java/com/olehprukhnytskyi/macrotrackercloudgateway/util/CustomHeaders.java
@@ -3,5 +3,4 @@ package com.olehprukhnytskyi.macrotrackercloudgateway.util;
 public class CustomHeaders {
     public static final String X_USER_ID = "X-User-Id";
     public static final String X_REQUEST_ID = "X-Request-Id";
-    public static final String X_TRACE_ID = "X-Trace-Id";
 }
