33010a5af526b5cf9be3d7248be739a3328a2534
Oleg Pruh <olegpruh1@gmail.com>
2025-12-06 01:04:59 +0000

    Fix: rate limiter properties

--- src/main/java/com/olehprukhnytskyi/macrotrackercloudgateway/filter/CustomHeadersFilter.java
+++ src/main/java/com/olehprukhnytskyi/macrotrackercloudgateway/filter/CustomHeadersFilter.java
@@ -3,6 +3,7 @@ package com.olehprukhnytskyi.macrotrackercloudgateway.filter;
 import com.olehprukhnytskyi.macrotrackercloudgateway.util.CustomHeaders;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContext;
@@ -12,7 +13,7 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
-public class CustomHeadersFilter implements GlobalFilter {
+public class CustomHeadersFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         return ReactiveSecurityContextHolder.getContext()
@@ -28,5 +29,10 @@ public class CustomHeadersFilter implements GlobalFilter {
                 })
                 .defaultIfEmpty(exchange)
                 .flatMap(chain::filter);
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
     }
 }
